d4fc6d4afd8c46db688e03a2d55465548f2dec7b
Vincent Nguyen <vpvnguyen@gmail.com>
2021-02-05 21:32:00 -0800

    removed default react styles

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { TitleComponent, Title } from "./components/Title";
+import { Description, StyledDescription } from "./components/Description";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TitleComponent title="Title - styled element wrapper" />
+      <Title>Title - imported and use styled element wrapper</Title>
+
+      <StyledDescription>Description - wrapped by styles</StyledDescription>
+      <Description>Description - normal</Description>
     </div>
   );
 }
