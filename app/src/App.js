3a527f16b229a6718149dd36e67756289ef15de9
Vincent Nguyen <vpvnguyen@gmail.com>
2021-02-06 10:21:36 -0800

    removed css files and created global style

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { Wrapper } from "./components/Wrapper";
 import { TitleComponent, Title } from "./components/Title";
 import { Description, StyledDescription } from "./components/Description";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <Wrapper>
       <TitleComponent title="Title - styled element wrapper" />
       <Title>Title - imported and use styled element wrapper</Title>
 
       <StyledDescription>Description - wrapped by styles</StyledDescription>
       <Description>Description - normal</Description>
-    </div>
+    </Wrapper>
   );
-}
+};
 
 export default App;
